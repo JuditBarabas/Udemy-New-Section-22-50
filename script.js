d491bebb7cc3394b7dfda22341b30f93174abe95
JuditBarabas <62601033+JuditBarabas@users.noreply.github.com>
2020-10-27 09:41:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function allEvens (arr) {
+    return arr.every(num => num % 2 === 0);
+}
+
+console.log('Test:');
+console.log(allEvens([2, 4, 6, 8]));
+console.log(allEvens([1, 4, 6, 8]));
+console.log(allEvens([1, 2, 3]));
